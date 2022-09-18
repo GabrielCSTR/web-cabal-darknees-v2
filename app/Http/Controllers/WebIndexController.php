a025970bc58c0c8a54a6edb86b9b9a124925edef
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2022-09-18 12:18:51 -0300

    ajust home dados

--- app/Http/Controllers/WebIndexController.php
+++ app/Http/Controllers/WebIndexController.php
@@ -16,35 +16,36 @@ class WebIndexController extends Controller
     public function index()
     {
         // ranking
-        $rankings = cabalCharacter::where('Nation', '!=', '3')
-                                //->where('LEV', '<', '200')
-                                ->whereRaw('name not like ?',"%HYPE%")
-                                ->whereRaw('name not like ?',"%Vara%")
-                                ->whereRaw('name not like ?',"%Dexter%")
-                                ->whereRaw('name not like ?',"%Testando%")
-                                ->whereRaw('name not like ?',"%teste%")
-                                ->orderBy('LEV', 'DESC')
-                                ->orderBy('Reputation', 'DESC')
-                                ->take(10)
-                                ->get();
-        // guilds
-        $guilds = cabalGuild::orderBy('Point', 'DESC')
-                                ->whereRaw('GuildName not like ?',"%STAFF%")
-                                ->whereRaw('GuildName not like ?',"%HYPE%")
-                                ->take(10)
-                                ->get();
-        // count player on
-        $playersON = cabalCharacter::where('Login', '1')
-                                ->count();
+        // $rankings = cabalCharacter::where('Nation', '!=', '3')
+        //                         //->where('LEV', '<', '200')
+        //                         ->whereRaw('name not like ?',"%HYPE%")
+        //                         ->whereRaw('name not like ?',"%Vara%")
+        //                         ->whereRaw('name not like ?',"%Dexter%")
+        //                         ->whereRaw('name not like ?',"%Testando%")
+        //                         ->whereRaw('name not like ?',"%teste%")
+        //                         ->orderBy('LEV', 'DESC')
+        //                         ->orderBy('Reputation', 'DESC')
+        //                         ->take(10)
+        //                         ->get();
+        // // guilds
+        // $guilds = cabalGuild::orderBy('Point', 'DESC')
+        //                         ->whereRaw('GuildName not like ?',"%STAFF%")
+        //                         ->whereRaw('GuildName not like ?',"%HYPE%")
+        //                         ->take(10)
+        //                         ->get();
+        // // count player on
+        // $playersON = cabalCharacter::where('Login', '1')
+        //                         ->count();
 
-        $playerAlzs = cabalCharacter::orderBy('Alz', 'DESC')
-                                    ->whereRaw('name not like ?',"%HYPE%")
-                                    ->whereRaw('name not like ?',"%[DEV]%")
-                                    ->whereRaw('name not like ?',"%[GM]%")
-                                    ->take(10)
-                                    ->get();
+        // $playerAlzs = cabalCharacter::orderBy('Alz', 'DESC')
+        //                             ->whereRaw('name not like ?',"%HYPE%")
+        //                             ->whereRaw('name not like ?',"%[DEV]%")
+        //                             ->whereRaw('name not like ?',"%[GM]%")
+        //                             ->take(10)
+        //                             ->get();
 
-        return view('web.index', compact('rankings', 'guilds', 'playersON', 'playerAlzs'));
+        // return view('web.index', compact('rankings', 'guilds', 'playersON', 'playerAlzs'));
+        return view('web.index');
     }
 
     public function download()
